090f71c76e6e62f82512641f4615d6e5828ab0f2
David Demcak <ddemcak@gmail.com>
2021-12-01 22:00:45 +0100

    Comments in code. Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# WeekDayCalculator
+# Week Day Calculator
 Simple script capable of listing work weeks and week numbers in a year.
+
+## Parameters
+- Year, for example "2022".
+- Date of first Monday in the year. For example "3".
--- WeekDateCalculator.ps1
+++ WeekDateCalculator.ps1
@@ -2,7 +2,7 @@ Write-Host '------------------------------' -ForegroundColor DarkGray
 Write-Host '    Week Date Calculator      ' -ForegroundColor DarkGray
 Write-Host '------------------------------' -ForegroundColor DarkGray
 
-
+# Check whether user provided 2 parameters.
 If ($args.Count -ne 2)
 {
     $year = Read-Host -Prompt "Please provide a year"
@@ -18,11 +18,15 @@ Else
 Write-Host 'Year:' $year -ForegroundColor Yellow
 Write-Host '1st Monday:' $day'. 1.' $year -ForegroundColor Yellow
 
+# Prepare date Monday variable.
+$date = (Get-Date -Day $day -Month 1 -Year $year -Hour 0 -Minute 0 -Second 0)
+
+# Results go here.
 Write-Host
 Write-Host 'Results:' -ForegroundColor Green
 
-$date = (Get-Date -Day $day -Month 1 -Year $year -Hour 0 -Minute 0 -Second 0)
-
+# Go in a loop until YEAR is still the same and print:
+# week number, day of Monday and Friday, plus add some brackets.
 $weekNumber = 1
 while ($date.Year -eq $year) {
     Write-Host $weekNumber'. KT (' -NoNewline -ForegroundColor Green
